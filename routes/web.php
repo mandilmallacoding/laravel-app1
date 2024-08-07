45c50bc6772ee7fefa7cc8aafa6ee36a7f384277
Mandil Malla <mandilmalla.cf@gmail.com>
2024-08-07 22:09:42 +0900

    make controller

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
+use App\Http\Controllers\Demo\DemoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
+// Route::get('/about', function () {
+//     return view('about');
+// });
+
+Route::controller(DemoController::class)->group(function () {
+    Route::get('/about', 'index');
+
 });
